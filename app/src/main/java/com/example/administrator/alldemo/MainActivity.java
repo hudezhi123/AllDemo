97848cfb16e5ce969d3451a0f0e4c92d2025db97
hudezhi <18401541305@163.com>
2018-08-20 14:09:51 +0800

    测试TEST

--- app/src/main/java/com/example/administrator/alldemo/MainActivity.java
+++ app/src/main/java/com/example/administrator/alldemo/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int TEST = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
